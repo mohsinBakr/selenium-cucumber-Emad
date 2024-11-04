5f16fcca3d10427f48c7e938abb6d699190641fb
mohsinBakr <mohsin.bakr@gmail.com>
2024-11-05 01:23:29 +0200

    ad

---
+++ src/test/resources/Reports/test-output-thread/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "cc6c90bc-2771-4064-a9b2-7e66fdc8d1a7",
+    "feature": "Search for product",
+    "scenario": "Go to landing page and search for \"Nokia\" then assert that the result is: \"Nokia Lumia 1020\"",
+    "start": 1730762560322,
+    "group": 30,
+    "content": "",
+    "tags": "@nopcom_searchforproduct,",
+    "end": 1730762570813,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 30,
+    "content": "Thread[#30,TestNG-PoolService-0,5,main]"
+  }
+]);
+});

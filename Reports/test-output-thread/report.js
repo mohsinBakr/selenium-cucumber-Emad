a0d1d51866520936ac9ae60e7db4aaf036af1a61
mohsinBakr <mohsin.bakr@gmail.com>
2024-11-07 21:36:46 +0200

    n

---
+++ Reports/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"b720170c-5fbf-4572-9158-515cf85b64b3","feature":"Verify Swica Landing Page","scenario":"Go to SWICA landing page and verify landing page elements existance","start":1731008002451,"group":31,"content":"","tags":"@swica_portal_landing,","end":1731008068126,"className":"failed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":31,"content":"Thread[#31,TestNG-PoolService-1,5,main]"}]);
+});

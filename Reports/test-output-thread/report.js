5dbf38ebf2e439ff144c1276d96d08f6b4b2e508
mohsinBakr <mohsin.bakr@gmail.com>
2024-11-07 20:40:15 +0200

    zxc

--- Reports/test-output-thread/report.js
+++ Reports/test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"9e735a12-ed4b-4382-a11b-9b68d24b3fc3","feature":"Search for product","scenario":"Go to SWICA landing page and verify landing page elements existance","start":1731004595125,"group":30,"content":"","tags":"@swica_portal_landing,","end":1731004660747,"className":"failed"}]);
-CucumberHTML.timelineGroups.pushArray([{"id":30,"content":"Thread[#30,TestNG-PoolService-1,5,main]"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"3490fe67-1799-4994-a830-90f3938cef29","feature":"Search for product","scenario":"Go to SWICA landing page and verify landing page elements existance","start":1731004707987,"group":29,"content":"","tags":"@swica_portal_landing,","end":1731004773609,"className":"failed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":29,"content":"Thread[#29,TestNG-PoolService-1,5,main]"}]);
 });
